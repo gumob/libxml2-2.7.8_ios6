01d916e010565a5d6d99a8c2f7d986dd665ead38
gumob <account@gumob.com>
2013-03-03 16:00:42 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@
 # 
 # Build thread safe libxml2 for iOS 6
 # 
-# 1) Download libxml2-2.7.8<br/>
-#    ftp://xmlsoft.org/libxml2/libxml2-2.7.8.tar.gz<br/>
-# 2) Run build.sh on unzipped libxml2-2.7.8 directory<br/>
-# 3) Copy libxml2.a and header files to your project directory<br/>
-# 4) Add Header Search Path<br/>
-#    Example: $(SRCROOT)/Submodules/libxml2-2.7.8_ios6<br/>
+# 1) Download libxml2-2.7.8
+#    ftp://xmlsoft.org/libxml2/libxml2-2.7.8.tar.gz
+# 2) Run build.sh on unzipped libxml2-2.7.8 directory
+# 3) Copy libxml2.a and header files to your project directory
+# 4) Add Header Search Path
+#    Example: $(SRCROOT)/Submodules/libxml2-2.7.8_ios6
 # 
 # Referenced
-# http://coin-c.tumblr.com/post/18063869172/thread-safe-xmllib2<br/>
-# http://pastie.org/3429938<br/>
+# http://coin-c.tumblr.com/post/18063869172/thread-safe-xmllib2
+# http://pastie.org/3429938
 #
 
 GLOBAL_OUTDIR="`pwd`/build"
